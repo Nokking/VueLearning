631b6c6ea71a697f5de29b5bfdc95befc0582b1b
Nokking <japaneseplay1994@gmail.com>
2019-07-30 17:09:02 +0900

    fix index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,7 @@
   <script src="{{ mix('js/app.js') }}" defer></script>
 
   <!-- Fonts -->
-  <link rel="dns-prefetch" href="//fonts.gstatic.com">
+  <link rel='stylesheet' id='x-google-fonts-css' href='//fonts.googleapis.com/css?family=Open+Sans%3A400%2C400italic%2C700%2C700italic%2C600%7CLato%3A700&subset=latin%2Clatin-ext&ver=5.2.5' type='text/css' media='all' />
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Merriweather|Roboto:400">
   <link rel="stylesheet" href="https://unpkg.com/ionicons@4.2.2/dist/css/ionicons.min.css">
 
