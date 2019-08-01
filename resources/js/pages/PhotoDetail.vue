f4d7df90a3f243af274598a3fb3a775a7ddb94fc
Nokking <japaneseplay1994@gmail.com>
2019-08-01 17:41:59 +0900

    add tweet button

--- resources/js/pages/PhotoDetail.vue
+++ resources/js/pages/PhotoDetail.vue
@@ -45,6 +45,8 @@
         </li>
       </ul>
       <p v-else>No comments yet.</p>
+      <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false">Tweet</a>
+      <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
       <form v-if="isLogin" @submit.prevent="addComment" class="form">
         <div v-if="commentErrors" class="errors">
           <ul v-if="commentErrors.content">
